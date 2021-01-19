a6ddc5dce2dff2639d705be00d70aa4adc1dd18b
Sejal Khanna <67584092+sejalllkhanna@users.noreply.github.com>
2021-01-19 10:45:08 +0530

    Add files via upload

---
+++ Realm App/Shared/Realm_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_AppApp.swift
+//  Shared
+//
+//  Created by Mac on 05/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Realm_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
